8ed861902e8cb6959e49f7cb20fcf39bec8ee10e
Albet Novendo <76980465+albetnov@users.noreply.github.com>
2024-07-29 11:04:52 +0700

    Delete dot_config/nvim/after/plugin directory

--- dot_config/nvim/after/plugin/workspace.lua
+++
@@ -1,8 +0,0 @@
--- get the current working directory
-local cwd = vim.fn.getcwd()
-
--- if cwd equals Work, disable formatting and linting
-if string.match(cwd, 'Work/') then
-  vim.g.disable_format = true
-  vim.g.disable_lint = true
-end
