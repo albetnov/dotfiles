77e0e8df988269af7976c9e04c5518df9ab2ba99
albetnov <ryxinfrvr@gmail.com>
2026-08-17 11:36:11 +0700

    Update .config/fish/config.fish Update .config/fish/fish_variables Update .config/ghostty/themes/noctalia Update .config/hypr/hyprland.conf Update .config/hypr/noctalia/noctalia-colors.conf Update .config/noctalia/colors.json Update .config/noctalia/plugins/network-indicator/BarWidget.qml Update .config/noctalia/plugins/network-indicator/README.md Update .config/noctalia/plugins/network-indicator/Settings.qml Update .config/noctalia/plugins/network-indicator/i18n/de.json Update .config/noctalia/plugins/network-indicator/i18n/en.json Update .config/noctalia/plugins/network-indicator/i18n/es.json Update .config/noctalia/plugins/network-indicator/i18n/fr.json Update .config/noctalia/plugins/network-indicator/i18n/hu.json Update .config/noctalia/plugins/network-indicator/i18n/it.json Update .config/noctalia/plugins/network-indicator/i18n/ja.json Update .config/noctalia/plugins/network-indicator/i18n/ku.json Update .config/noctalia/plugins/network-indicator/i18n/nl.json Update .config/noctalia/plugins/network-indicator/i18n/pl.json Update .config/noctalia/plugins/network-indicator/i18n/pt.json Update .config/noctalia/plugins/network-indicator/i18n/ru.json Update .config/noctalia/plugins/network-indicator/i18n/tr.json Update .config/noctalia/plugins/network-indicator/i18n/uk-UA.json Update .config/noctalia/plugins/network-indicator/i18n/zh-CN.json Update .config/noctalia/plugins/network-indicator/manifest.json Update .config/noctalia/plugins/network-indicator/preview.png Update .config/noctalia/plugins/privacy-indicator/Main.qml Update .config/noctalia/plugins/privacy-indicator/Settings.qml Update .config/noctalia/plugins/privacy-indicator/i18n/fr.json Update .config/noctalia/plugins/privacy-indicator/manifest.json Update .config/noctalia/settings.json Update .config/steamtinkerlaunch/default_template.conf Update .config/steamtinkerlaunch/gamecfgs/id/31337.conf Update .config/steamtinkerlaunch/global.conf Update .config/steamtinkerlaunch/logs/gamelaunch/id/1222680.log Update .config/steamtinkerlaunch/logs/steamtinkerlaunch/id/1222680.log Update .config/steamtinkerlaunch/logs/steamtinkerlaunch/id/31337.log Update .config/steamtinkerlaunch/logs/steamtinkerlaunch/lastrun.txt Update .config/steamtinkerlaunch/meta/id/general/1222680.conf Update .config/steamtinkerlaunch/url.conf

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -11,7 +11,7 @@ end
 
 fish_add_path ~/.local/bin
 fish_add_path ~/.nix-profile/bin
-fish_add_path ~/.bun/bin
+set -Ux CHROME_EXECUTABLE brave
 fish_add_path ~/tools/flutter/flutter/bin
 
 alias ps 'procs'
@@ -21,3 +21,18 @@ alias aigoo 'nvim'
 zoxide init fish | source
 
 starship init fish | source
+
+# opencode
+fish_add_path /home/albetnv/.opencode/bin
+
+# pnpm
+set -gx PNPM_HOME "/home/albetnv/.local/share/pnpm"
+if not string match -q -- "$PNPM_HOME/bin" $PATH
+  set -gx PATH "$PNPM_HOME/bin" $PATH
+end
+# pnpm end
+/home/albetnv/.local/bin/mise activate fish | source
+
+
+# Added by Antigravity CLI installer
+set -gx PATH "/home/albetnv/.local/bin" $PATH
